d747a775318cef91ee9772cc57e63db42220ff81
tsymq <794083005@qq.com>
2019-08-20 19:56:55 +0800

    test

---
+++ java/cn/itcast/Test.java
@@ -0,0 +1,12 @@
+package cn.itcast;
+
+/**
+ * @author zs
+ * @time 2019/8/20 19:54
+ * @desc
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("你好，github");
+    }
+}
